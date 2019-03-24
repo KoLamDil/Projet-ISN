db3699b8bb6ae826fe9105328909d3c0f2796313
KoLamDil <48588312+KoLamDil@users.noreply.github.com>
2019-03-24 17:37:59 +0100

    Add files via upload
    Add some JS
    Html : 
    - Add 4 buttons (+CSS)
    - Add SCROLL div

---
+++ humanity.js
@@ -0,0 +1,39 @@
+/* List objects
+var NOMOBJECT={
+    NomPrototype1: "ValeurPrototype1",
+    NomPrototype2: "ValeurPrototype2",
+    NomPrototype3: "ValeurPrototype3",
+}
+
+
+*/
+var planet = {
+    rank: "1",
+    population: "10",
+    woodDensity: "1",
+    ironDensity: "1",
+    stoneDensity: "1",
+    carboneDensity: "1",
+    petroleumDensity: "1",
+    leadDensity: "1"
+}
+var numExploration = 0;
+var explorationPoints = 0;
+var thecnologyPoints = 0;
+var 
+
+function popByClick () {
+    planet.population = planet.population + ;
+    document.getElementById("populationDisplay").innerHTML = planet.population + " Humans";
+    return planet.population;
+}
+
+
+
+document.getElementById("").innerHTML = "" + ; 
+document.getElementById("").innerHTML = "" + ; 
+document.getElementById("").innerHTML = "" + ; 
+document.getElementById("").innerHTML = "" + ; 
+document.getElementById("").innerHTML = "" + ; 
+document.getElementById("").innerHTML = "" + ; 
+document.getElementById("planetNumDisplay").innerHTML = "Planet : " + numExploration;
